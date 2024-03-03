ddcfd3cfb9e943f095e0a760ecaefc87efa880ff
sachinsav <sachin.saw.13@gmail.com>
2024-03-03 17:58:05 +0530

    route for client added

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const userRoute = require('./routes/UserRoutes');
 const foodRoute = require('./routes/foodRoutes');
 const orderRoute = require('./routes/OrderRoutes');
 const cors = require('cors');
+const path = require('path');
 const app = express()
 const port = 5000
 
 app.use(cors())
-app.get("/", (req, res) => {
-    res.send("hello World");
-});
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.json());
 app.use("/api", userRoute);
 app.use("/api", foodRoute);
 app.use("/api", orderRoute);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(port, ()=>{
     console.log(`server is running on port  ${port}`)
 })
